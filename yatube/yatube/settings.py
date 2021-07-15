be2531fc38bd687928b2317c0531bbf018325b60
ViktoriaPashkovskaja <pashkovskajava@yandex.ru>
2021-07-15 20:29:49 +0300

    Исправлено количество записей

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -4,9 +4,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    # 'www.yatubeya.pythonanywhere.com',
+    # 'yatubeya.pythonanywhere.com',
     "localhost",
     "127.0.0.1",
     "[::1]",
@@ -34,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'yatube.urls'
